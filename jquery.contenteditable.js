dda706dfcaa24f6d20e4485b2d52a8a99bedb154
Makis Tracend <makis.tracend@gmail.com>
2012-08-20 22:49:55 -0700

    Initial commit

--- jquery.contenteditable.js
+++ jquery.contenteditable.js
@@ -1,3 +1,76 @@
 // jQuery contentEditable() plugin
-// created by Makis Tracend (@tracend)
+//
+// Created by: Makis Tracend (@tracend)
+// Issue: http://stackoverflow.com/a/6263537
 
+(function( $ ){
+
+  var methods = {
+     init : function( options ) {
+		
+       return this.each(function(){
+         
+			var $this = $(this);
+			
+			//reset any previous change events set
+			$this.unbind("change");
+			
+			$this.find('[contenteditable]').each(function(){
+			
+				$(this).contentEditable("field", $this);
+				
+			});
+			
+	   });
+     },
+     field : function( parent ) {
+	 
+	 	return this.each(function(){
+		
+         	var $this = $(this);
+			// setting the key based on an attribute available on the same level as 'contentEditable'
+			var key = $this.attr("data-key");
+			// add triggers
+			$this.live('focus', function() {
+				var $this = $(this);
+				$this.data('enter', $this.html());
+				$this.data('before', $this.html());
+				return $this;
+			}).live('keyup paste', function() {
+				var $this = $(this);
+				var text = $this.html();
+				if ($this.data('before') !== text) {
+					$this.data('before', text);
+					var data = {};
+					data[key] = text;
+					parent.trigger({type: 'change', action : 'update', changed: data});
+				}
+				return $this;
+			}).live('blur', function() {
+				var $this = $(this);
+				var text = $this.html();
+				if ($this.data('enter') !== text) {
+					$this.data('enter', text);
+					var data = {};
+					data[key] = text;
+					parent.trigger({type: 'change', action : 'save', changed: data});
+				}
+				return $this;
+			})
+	 	});
+	 }
+  };
+
+  $.fn.contentEditable = function( method ) {
+    
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.contentEditable' );
+    }    
+  
+  };
+
+})( jQuery );
